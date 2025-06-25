3f3282c4721c27c90e5416aae451a6cd889dd3cb
Daniel Gruno <humbedooh@apache.org>
2025-06-25 08:37:12 +0200

    Update client_unblock.py, add email token feature
    This is still behind basic auth, so only infra can test it

--- endpoints/client_unblock.py
+++ endpoints/client_unblock.py
@@ -21,15 +21,40 @@ import operator
 import time
 import uuid
 import aiohttp.web
-
+import asfpy.messaging
+import email.utils
 """ client-side unblocking endpoint for Blocky/4"""
 
 # Infraction rules:
 # niceness -1 == You can unblock yourself by visiting from that IP
 # niceness -2 or above == You can't unblock yourself, send an email..
 
+RATE_LIMIT = 600  # You can only request an unblock email every 10 minutes per IP
+pending_unblocks = {}
+
 async def process(state: plugins.configuration.BlockyConfiguration, request, formdata: dict) -> dict:
     token = formdata.get('token', None)
+    email = formdata.get('email', None)
+    if email:
+        email_parsed = email.utils.parseaddr(email)
+        if not (len(email_parsed) == 2 and '@' in email_parsed[1]):
+            return aiohttp.web.Response(status=400, text=f"Invalid email address specified: {email}")
+        my_ip = request.headers["x-forwarded-for"]
+        entry = state.sqlite.fetchone("santalist", ip=my_ip)
+        if entry and email:
+            last_attempt = pending_unblocks.get(my_ip, 0)
+            if last_attempt < (time.time() - RATE_LIMIT):
+                token = entry["token"]
+                asfpy.messaging.mail(
+                    recipient = email,
+                    subject = f"Token for unblocking IP {my_ip}",
+                    message = f"Please visit the following URL to request an automatic unblock of your IP:\n https://blocky.apache.org/client_unblock?token={token}\nThis token can only be used once.\n"
+                )
+                return aiohttp.web.Response(status=200, text=f"Unblock token for {my_ip} has been sent to {email}")
+            else:
+                return aiohttp.web.Response(status=429, text=f"The IP address {my_ip} already has a pending unblock request, please wait a while beforoe trying again.")
+        return aiohttp.web.Response(status=410, text=f"I could not find any block entries for this IP address ({my_ip}). If you still believe you are blocked, reach out to abuse@infra.apache.org")
+                
     if token:
         my_ip = request.headers["x-forwarded-for"]
         entry = state.sqlite.fetchone("santalist", token=token)
